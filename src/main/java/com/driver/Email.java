4a223b29cef77f881c25bc0fedbad03f93b17e06
SoumyaMishra07 <soumyarmishra065@gmail.com>
2024-02-24 12:28:05 +0530

    added

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,5 +1,5 @@
 package com.driver;
-
+import java.util.*;
 public class Email {
 
     private String emailId;
@@ -10,6 +10,10 @@ public class Email {
         this.password = "Accio@123";
     }
 
+    public Email(Date date, String sender, String message) {
+
+    }
+
     public String getEmailId() {
         return emailId;
     }
@@ -18,12 +22,26 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
+    public boolean changePassword(String oldPassword, String newPassword){
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        if (!oldPassword.equals(this.password)) {
+            return false;
+        }
+        if (newPassword.length() < 8 ||
+                !newPassword.matches(".*[A-Z].*") ||
+                !newPassword.matches(".*[a-z].*") ||
+                !newPassword.matches(".*\\d.*") ||
+                !newPassword.matches(".*[^A-Za-z0-9].*")) {
+            return false;
+        }
+
+        this.password = newPassword;
+        return true;
     }
 }
